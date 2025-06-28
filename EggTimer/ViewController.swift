c6bff460ac78f275d10215b5f8b58ea97627aeaa
23ji <isj4194@naver.com>
2025-06-28 18:32:22 +0900

    버튼 클릭에 따른 프로그레스 바 설정

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,15 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-  var eggTime = ["soft": 5, "medium": 7, "hard": 12]
+  
+  var eggTime = ["Soft": 5, "Medium": 7, "Hard": 12]
+  var totalTime = 0
+  var progressTime = 0
   
   @IBOutlet weak var progressBar: UIProgressView!
   
-  @IBOutlet weak var softButton: UIButton!
+  @IBOutlet weak var hardnessButton: UIButton!
   
   @IBAction func hardnessSelected(_ sender: UIButton) {
-   
+    guard let eggText = sender.titleLabel?.text else { return }
+    guard let totalTime = eggTime[sender.currentTitle!] else { return }
+    
+    self.progressBar.progress = Float(totalTime)
+    
+    Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+      
+      self.progressTime += 1
+      
+//      if self.progressTime >= self.totalTime {
+//        self.timer?.invalidate()
+//        self.timer = nil
+//      }
+    }
+    
   }
-  
 }
